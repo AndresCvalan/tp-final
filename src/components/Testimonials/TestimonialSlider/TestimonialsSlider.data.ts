d11f583ed35e9b002a301cd38294833836bbe7e0
Andres Corvalan <nttadc@gmail.com>
2023-10-02 17:43:43 -0300

    first commit

---
+++ src/components/Testimonials/TestimonialSlider/TestimonialsSlider.data.ts
@@ -0,0 +1,30 @@
+export const testimonialsData = [
+  {
+    id: 0,
+    name: "Juan Paloma",
+    testimonial:
+      "Trabajamos con AndyDev para el diseño de nuestro sitio web y estamos muy contentos con los resultados.fue muy profesional y comprensivo, y nos ayudaron a crear un sitio web que cumple con nuestras necesidades y objetivos. .",
+    image: "profile1.png",
+  },
+  {
+    id: 1,
+    name: "Sol Smith",
+    testimonial:
+      "AndyDev hizo un trabajo fantástico con nuestro sitio web. El diseño es moderno y atractivo, y el sitio web es fácil de usar. También apreciamos su atención al detalle y su disposición a trabajar con nosotros para crear un sitio web que se ajustara a nuestras necesidades específicas.",
+    image: "profile2.png",
+  },
+  {
+    id: 2,
+    name: "Mauro Rodriguez",
+    testimonial:
+      "AndyDev nos ayudó a transformar nuestro sitio web en una herramienta poderosa para nuestro negocio. El diseño es elegante y profesional, y el sitio web es fácil de navegar.",
+    image: "profile3.png",
+  },
+  {
+    id: 3,
+    name: "José Zoliban",
+    testimonial:
+      "AndyDev fue un placer trabajar con ellos. El equipo fue muy comunicativo y receptivo a nuestras sugerencias. El sitio web que crearon es exactamente lo que estábamos buscando, y estamos muy contentos con los resultados.",
+    image: "profile4.png",
+  },
+];
